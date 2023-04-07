00482e5d6d332db015fd10eda12b4f9592c60dbb
alex lieberman <alex.lieberman182@gmail.com>
2023-04-07 16:59:55 -0700

    fixed the crud

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,13 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/nicknames' do
-    nickname = Nickname.create(nickname: params [:nickname], athlete_id: params[:athlete_id])
+    nickname = Nickname.create(nickname: params[:nickname], athlete_id: params[:athlete_id])
     nickname.to_json
   end
   
   patch '/nicknames/:id' do
     nickname = Nickname.find(params[:id])
-    nickname.update(nickname: params [:nickname], athlete_id: params[:athlete_id])
+    nickname.update(nickname: params[:nickname], athlete_id: params[:athlete_id])
     nickname.to_json
+  end 
 end
