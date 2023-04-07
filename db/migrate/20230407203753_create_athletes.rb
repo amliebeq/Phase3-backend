54e3444004cbab6e1baf92256e1dd5f0fc04bddf
alex lieberman <alex.lieberman182@gmail.com>
2023-04-07 13:47:17 -0700

    created tables

--- db/migrate/20230407203753_create_athletes.rb
+++ db/migrate/20230407203753_create_athletes.rb
@@ -1,4 +1,10 @@
 class CreateAthletes < ActiveRecord::Migration[6.1]
   def change
+    create_table :athletes do |t|
+      t.string :name
+      t.string :sport
+      t.string :reference_url
+      t.timestamps
+    end
   end
 end
--- db/migrate/20230407203810_create_nicknames.rb
+++ db/migrate/20230407203810_create_nicknames.rb
@@ -1,4 +1,9 @@
 class CreateNicknames < ActiveRecord::Migration[6.1]
   def change
+    create_table :nicknames do |t|          
+      t.string :nickname
+      t.integer :athlete_id
+      t.timestamps
+    end
   end
 end
